acb3b428d73255b391d8c6181b708e7972162934
harryninja <harold_illert@outlook.com>
2023-04-17 14:01:16 -0300

    Diablo clone 2 yay again

---
+++ Assets/Game/Scripts/Combat/Fighter.cs
@@ -0,0 +1,177 @@
+﻿using RPG.Core;
+using RPG.Movement;
+using RPG.Saving;
+using UnityEngine;
+
+//uses:
+//actionScheduler
+//mover
+//health
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction, ISaveable
+    {
+        //these properties relate to weapons and need to be moved
+        [Header("Fighter Stats")]
+        //[SerializeField] private float weaponRange = 2f;
+        //[SerializeField] private float timeBetweenAttacks;
+        //[SerializeField] private float weaponDamage;
+        
+        [Header("Weapon")]
+        [SerializeField] private Transform rightHandPosition = null;
+        [SerializeField] private Transform leftHandPosition = null;
+        [SerializeField] private Weapon defaultWeapon = null;
+        [SerializeField] private Weapon equippedWeapon = null;
+
+        [Header("")]
+        public float timer = 20;
+        
+        //references 
+        public Health target; //serialized for debug
+        
+        //cached
+        private Mover mover;
+        private ActionScheduler actionScheduler;
+        private Animator anim;
+      
+        void Awake()
+        {
+             mover = GetComponent<Mover>();
+             actionScheduler = GetComponent<ActionScheduler>();
+             anim = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            if(!equippedWeapon)
+                EquipWeapon(defaultWeapon);
+        }
+
+        public void EquipWeapon(Weapon weapon)
+        {
+            equippedWeapon = weapon;
+            weapon.SpawnToPlayer(rightHandPosition, leftHandPosition, anim);
+        }
+
+        public void UnequipWeapon()
+        {
+            if (equippedWeapon == defaultWeapon) return;
+            equippedWeapon.DestroyWeaponOnPlayer(rightHandPosition, leftHandPosition, anim);
+            EquipWeapon(defaultWeapon);
+        }
+        
+        
+        void Update()
+        {
+            
+            
+            timer += Time.deltaTime;
+            
+            
+            if (!target)
+                return;
+            
+            
+            
+            if (!InRange())
+            {
+                //player has not reached enemy
+                    mover.MoveTo(target.transform.position);
+            }
+            else //witihn range of target, safe to attack
+            {
+                    
+                    transform.LookAt(target.transform);
+                    mover.Cancel();
+                    
+                    //do attacking stuff here
+                    AttackBehavior();
+
+            }
+            
+        }
+        
+
+        private bool InRange()
+        {
+            var distance =  Mathf.Abs(Vector3.Distance(transform.position, target.transform.position));
+            return distance < equippedWeapon.GetWeaponRange();
+        }
+        
+
+        private void AttackBehavior()
+        {
+            if (target.IsDead())
+            {
+                Cancel();
+                actionScheduler.CancelAction();
+                
+            }
+
+            else if (timer >equippedWeapon.GetTimeBetweenAttacks())
+            {
+                anim.ResetTrigger("stopAttack");
+                anim.SetTrigger("attack");
+
+                if (equippedWeapon.IsRanged())
+                    equippedWeapon.SpawnProjectile(target.transform, rightHandPosition, leftHandPosition);
+                
+
+                timer = 0;
+
+            }
+        }
+
+        public void Cancel()
+        {
+           
+            target = null;
+            anim.SetTrigger("stopAttack");
+
+        }
+       
+        public void Attack(GameObject combatTarget)
+        {
+            actionScheduler.StartAction(this);
+            target = combatTarget.GetComponent<Health>();
+        }
+
+        public bool CanAttack(GameObject target)
+        {
+            return target && !target.GetComponent<Health>().IsDead();
+        }
+        
+        //animation event
+        void Hit()
+        {
+            if (!target) return;
+            
+            target.TakeDamage(equippedWeapon.GetWeaponDamage());
+            
+        }
+        
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            if(equippedWeapon)
+                Gizmos.DrawWireSphere(transform.position,equippedWeapon.GetWeaponRange());
+        }
+
+        public void SetTarget(Health other)
+        {
+            target = other;
+        }
+
+        public object CaptureState()
+        {
+            return equippedWeapon.name;
+        }
+
+        public void RestoreState(object state)
+        {
+            string weaponName = (string) state;
+            Weapon weapon = Resources.Load<Weapon>(weaponName);
+            EquipWeapon(weapon);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Combat/PickableWeapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using RPG.Combat;
+using RPG.Controller;
+using UnityEngine;
+
+public class PickableWeapon : MonoBehaviour
+{
+    [SerializeField] private Weapon weapon;
+    [SerializeField] private float respawnTime = 5f;
+    private void OnTriggerEnter(Collider other)
+    {
+        Fighter fighter = other.GetComponent<Fighter>();
+        if (fighter && other.GetComponent<PlayerController>())
+        {
+           
+            fighter.EquipWeapon(weapon);
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+    }
+
+    private IEnumerator HideForSeconds(float seconds)
+    {
+        ShowPickup(false);
+        yield return new WaitForSeconds(seconds);
+        ShowPickup(true);
+
+    }
+
+    private void ShowPickup(bool shouldShow)
+    {
+        //transform.Find("childname") returns the child with name childname
+        GetComponent<SphereCollider>().enabled = shouldShow;
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(shouldShow);
+        }
+    }
+
+   
+    
+}
